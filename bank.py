500066324e84aa00cc32934e5f6fda998ee8b552
test <test>
2024-12-04 13:16:05 +0700

    test

---
+++ bank.py
@@ -0,0 +1,17 @@
+def main():
+    greeting = input("Приветствие: ")
+    res = value(greeting)
+    print(f"${res}")
+
+def value(greeting):
+    greeting = greeting.lower()
+    if greeting.startswith("здравствуйте"):
+        return 0
+    elif greeting.startswith("з"):
+        return 20
+    else:
+        return 100
+
+if __name__=="__main__":
+    main()
+
